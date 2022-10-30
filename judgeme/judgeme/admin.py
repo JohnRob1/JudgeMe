d7c4aabae8aa49a272000dd165de9dcf2117b9b8
Kenny Park <parkkennypark@gmail.com>
2022-10-30 13:45:03 -0400

    Implementing JMUser model

--- judgeme/judgeme/admin.py
+++ judgeme/judgeme/admin.py
@@ -1,5 +1,30 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-from .models import User
+from .forms import JMUserCreationForm, JMUserChangeForm
+from .models import JMUser
 
-admin.site.register(User)
+admin.site.register(JMUser)
+
+
+class JMUserAdmin(UserAdmin):
+    add_form = JMUserCreationForm
+    form = JMUserChangeForm
+    model = JMUser
+    list_display = ('username')
+    list_filter = ('username')
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active')}
+         ),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+
+
+admin.site.register(JMUser, JMUserAdmin)
