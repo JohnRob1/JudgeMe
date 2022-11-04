ec01a59f4ea14240225451a8dbf39824e1075dca
Kenny Park <parkkennypark@gmail.com>
2022-11-04 02:03:00 -0400

    Finished john's merge

--- judgeme/judgeme/views.py
+++ judgeme/judgeme/views.py
@@ -11,16 +11,7 @@ from .models import JMUser, Track
 
 import spotipy
 import os
-from spotipy import oauth2
-from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth
-
-SPOTIPY_CLIENT_ID = '1fba4b0df2fe49318273c0ab3aeb1d49'
-SPOTIPY_CLIENT_SECRET = '8d0bfdb045024e74bbdc22cd47c69588'
-SPOTIPY_REDIRECT_URI = 'http://127.0.0.1:8000/tutorial/'
-# https://developer.spotify.com/documentation/general/guides/authorization/scopes/ for scopes
-scope = 'user-top-read user-library-read playlist-read-private playlist-modify-public user-read-private user-read-email'
-username = ''
-darkmode = False
+import random
 
 
 def sign_in(request):
@@ -89,6 +80,85 @@ def judge(request):
     return render(request, 'judge.html')
 
 
+def result(request):
+
+    # Implement Comparison Algorithm
+    MusicTaste = 0
+
+    # Read in Correlation Factors into Hash
+    f = open('theme/static/genre_correlations', 'r')
+    lines = f.readlines()
+    genres_cf = {}
+    genres_amt = {}
+    for line in lines:
+        values = line.split(",")
+        values[1] = values[1].strip()
+        genres_cf[values[0].lower()] = values[1]
+    # Get genres of songs for weight
+    sp = get_spotify_object(request)
+    print("Trying api call")
+    pprint(sp.artist("0TnOYISbd1XYRBk9myaseg"))
+
+    i = 0
+    genres = []
+    for track in sp.current_user_top_tracks(1).get("items"):
+        song_uri = track.get("uri")
+        artist_id = sp.track(song_uri).get("artists")[0].get("id")
+        artist = sp.artist(artist_id)
+        print("Working...")
+        genres = artist.get("genres")
+        for genre in genres:
+            added_to_dict = False
+            if genres_cf.get(genre, None) != None:
+                added_to_dict = True
+                if genres_amt.get(genre, None) == None:
+                    genres_amt[genre] = 0
+                else:
+                    genres_amt[genre] = genres_amt.get(genre) + 1
+            else:
+                # Check is the genre given is just the subtype of a genre in the correlation values
+                split = genre.split()
+                for word in split:
+                    if genres_cf.get(word, None) != None:
+                        added_to_dict = True
+                        if genres_amt.get(word, None) == None:
+                            genres_amt[word] = 0
+                        else:
+                            genres_amt[word] = genres_amt.get(word) + 1
+            # Genre has no personality correlation
+            # CF = 3/36
+            if added_to_dict is False:
+                if genres_amt.get(genre, None) == None:
+                    genres_amt[genre] = 0
+                else:
+                    genres_amt[genre] = genres_amt.get(genre) + 1
+                genres_cf[genre] = 3/36
+
+    # Calculate MusicTaste
+    for genre in genres_amt.keys():
+        MusicTaste = MusicTaste + \
+            ((float(genres_amt.get(genre, 0) / 100))
+             * float(genres_cf.get(genre, 0)))
+    request.user.music_taste = MusicTaste
+
+    MusicTaste = 0.46
+    print(MusicTaste)
+
+    f = open('theme/static/light_mode_gifs/insults.txt', 'r')
+    lines = f.readlines()
+    r = random.randint(0, len(lines) - 1)
+    line = lines[r]
+    sh = line.split(", ")
+
+    context = {
+        'src': sh[0],
+        'height': sh[1],
+        'href': sh[2],
+        'MusicTaste': MusicTaste
+    }
+    return render(request, 'result.html', context)
+
+
 def profile(request):
     sp = get_spotify_object(request)
 
@@ -146,40 +216,14 @@ def homepage(request):
     if 'lightMode' in request.GET:
         darkmode = False
 
-    request_code = 0
-    if 'add-friend' in request.GET:
-        username = request.GET['add-friend']
-        print("trying to add:", username)
-
-        try:
-            user = JMUser.objects.get(username=username)
-            request.user.friends.add(user)
-            print("friend added.")
-            request_code = 1
-        except ObjectDoesNotExist:
-            print("doesn't exist!!")
-            request_code = 2
-
-    if 'remove-friend' in request.GET:
-        username = request.GET['remove-friend']
-        print("trying to remove:", username)
-        try:
-            user = JMUser.objects.get(username=username)
-            request.user.friends.remove(user)
-            print("friend removed.")
-            request_code = 3
-        except ObjectDoesNotExist:
-            print("doesn't exist!!")
-            request_code = 4
-
-    while (True):
-        result = sp.current_user_playlists(limit=50, offset=iterations*50)
-        items = result.get('items')
-        if (len(items) == 0):
-            break
-        iterations += 1
-        for playlist in items:
-            playlists.append(playlist)
+    # while (True):
+    #     result = sp.current_user_playlists(limit=50, offset=iterations*50)
+    #     items = result.get('items')
+    #     if (len(items) == 0):
+    #         break
+    #     iterations += 1
+    #     for playlist in items:
+    #         playlists.append(playlist)
 
     count = 0
     for item in playlists:
@@ -204,16 +248,11 @@ def homepage(request):
 
     context = {'user': request.user,
                'friendcount': request.user.friends.count(), 'playlist_count': count}
-    context['user'] = request.user
-    context['friends'] = request.user.friends.all()
-    context['request_code'] = request_code
-
     context['friend1'] = friend1
     context['friend2'] = friend2
     context['friend3'] = friend3
     context["bg_color"] = "[#322c3d]"
     context["bubble_color"] = "[#8e3d81]"
-
     return render(request, 'homepage.html', context)
 
 
@@ -294,7 +333,16 @@ def artist(request):
                     'album_titles': album_titles,
                     'image': image_artist,
                     'popularity': popularity,
+                    'name': name,
                 }
+                # context = {
+                #     'render_intro': False,
+                #     'top_tracks': ['Jane\'s song', 'Cupids Arrow', 'Fake Song 3', 'No Ideas', 'John Robinson is the best'],
+                #     'album_titles': ['Album Premier', 'Album deux: Springtime', 'Jayanthas Etude'],
+                #     'image': image_artist,
+                #     'popularity': popularity,
+                #     'name' : artist,
+                # }
                 return render(request, 'artist.html', context)
             return render(request, 'artist.html', {'error': True})
     context = {
